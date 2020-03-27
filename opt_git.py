56b8208426365ec6d8a8657a39a6b205e1f602a3
0x0u <m0zurillex@gmail.com>
2020-03-27 20:23:10 +0900

    コミットメッセージにランダムな絵文字を入れるようにする

--- opt_git.py
+++ opt_git.py
@@ -1,6 +1,9 @@
 import os
+import random
 from git import Repo
 from datetime import datetime, timedelta, timezone
+
+emoji = ["☀️", "❄️", "☁️", "⚡️", "☔️"]
 
 path = os.getcwd()
 repo = Repo(path)
@@ -8,6 +11,8 @@ repo = Repo(path)
 add = repo.index.add("README.md")
 jst = timezone(timedelta(hours=+9), "JST")
 today = datetime.now(jst).strftime("%Y-%m-%d")
-msg = "☀️ {}".format(today)
+num = random.randint(0, 4)
+msg = "{} {} {}".format(emoji[num], today, emoji[num])
 commit = repo.index.commit(msg)
 repo.git.push("origin", "master")
+
